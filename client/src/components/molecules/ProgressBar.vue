e6826f03c129a0e30fc8d87e5b7726765c35d873
jordyvanderhaegen <jordvand9@student.arteveldehs.be>
2019-07-22 13:43:13 +0200

    Add: progressbar via package

---
+++ client/src/components/molecules/ProgressBar.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="m-progress-bar"></div>
+</template>
+
+<style lang="scss">
+.m-progress-bar {
+  width: 100%;
+  height: 3px;
+  margin: 20px 0;
+}
+</style>
+
+<script>
+import { Line } from 'progressbar.js'
+export default {
+  name: 'm-progress-bar',
+  props: {
+    duration: {
+      type: Number,
+      required: true,
+    }
+  },
+  mounted() {
+    const line = new Line(this.$el, {
+      strokeWidth: 3,
+      duration: this.duration,
+      color: 'white',
+      trailColor: 'rgba(255, 255, 255, 0.6)',
+      trailWidth: 1,
+      svgStyle: {width: '100%', height: '100%'}
+    })
+    line.animate(1)
+  }
+}
+</script>
+
+
