e9fe88702a5feb4865a7e4544a22520cb2cad3ed
jordyvanderhaegen <jordvand9@student.arteveldehs.be>
2019-08-17 14:28:19 +0200

    Update: remove duplicate code

--- client/src/components/atoms/DeckLayer.vue
+++ client/src/components/atoms/DeckLayer.vue
@@ -6,7 +6,7 @@
 <script>
 import { Deck, FlyToInterpolator } from '@deck.gl/core';
 import { ScatterplotLayer } from '@deck.gl/layers';
-import { mapMutations, mapState } from 'vuex';
+import { mapMutations } from 'vuex';
 import * as d3 from 'd3';
 import * as pstimer from '@/assets/js/timer.js'
 import timelineDates from '@/assets/data/map.json';
@@ -26,16 +26,11 @@ export default {
       currentFrame: 0,
     };
   },
-  computed: {
-    ...mapState('timeline', ['map', 'deck', 'timer', 'activeDateId', 'cameraLocked', 'timelineDates']),
-  },
   mounted() {
     this.initDeck()
-    // this.loadGeodata()
   },
   methods: {
-    ...mapMutations('timeline', ['setDeck', 'setTimer', 'incrementActiveDateId', 'setTooltipPos']),
-    ...mapMutations('ui', ['toggleModal']),
+    ...mapMutations('timeline', ['setDeck']),
     initDeck() {
       const deck = new Deck({
         canvas: this.$refs.deck,
@@ -53,107 +48,6 @@ export default {
         },
       });
       this.setDeck(deck)
-    },
-    async loadGeodata() {
-      const timelineCurrentDate = this.timelineDates.find(item => item.id === this.activeDateId);
-      const timelineNextDate = this.timelineDates.find(item => item.id === this.activeDateId + 1);
-      const geodataCurrentDate = await import(`@/assets/data/${timelineCurrentDate.date}.json`);
-      const geodataNextDate = await import(`@/assets/data/${timelineNextDate.date}.json`);
-      this.geodataNextDate = geodataCurrentDate.features
-      this.geodataCurrentDate = geodataCurrentDate.features.map((feature) => {
-        // Destruct feature object
-        const {id, LN} = feature.properties;
-        // Get next point to determine interpolation
-        const featureNextDate = geodataNextDate.features.find(feature =>
-          // feature.properties.id == id && feature.properties.front == front
-          feature.properties.LN == LN
-        );
-        return {
-          ...feature,
-          interpolatePos: d3.geoInterpolate(feature.geometry.coordinates,
-          featureNextDate ? featureNextDate.geometry.coordinates : feature.geometry.coordinates
-          ),
-        };
-      });
-
-      if (this.cameraLocked) {
-        this.deck.setProps({
-          viewState: {
-            ...timelineCurrentDate.camera,
-            transitionInterpolator: new FlyToInterpolator(),
-            transitionDuration: 3000,
-            transitionEasing: d3.easeCubic
-          }
-        });
-      }
-
-      this.startAnimation()
-      this.incrementActiveDateId()
-    },
-    interpolateGeodata() {
-      if (this.currentFrame <= 1) {
-        this.geodataCurrentDate.forEach(feature => {
-          feature.geometry.coordinates = feature.interpolatePos(this.currentFrame);
-        });
-
-        this.deck.setProps({
-          layers: this.createScatterPlotterLayer(),
-          viewState: this.deck.viewState,
-        });
-
-        this.currentFrame += 0.005;
-        return
-      }
-
-      this.timer.stop();
-      this.loadGeodata();
-    },
-    startAnimation() {
-      if (this.timer) {
-        this.timer.stop();
-      }
-
-      this.currentFrame = 0;
-      // this.setTimer(d3.timer(this.interpolateGeodata))
-      this.setTimer(pstimer.pausableTimer(this.interpolateGeodata))
-    },
-    createScatterPlotterLayer() {
-      return new ScatterplotLayer({
-        id: 'scatterplot-layer',
-        data: [...this.geodataCurrentDate],
-        autoHighlight: true,
-        pickable: true,
-        radiusScale: 2,
-        radiusMinPixels: 2,
-        radiusMaxPixels: 6,
-        getPosition: d => [d.geometry.coordinates[0], d.geometry.coordinates[1]],
-        getFillColor: d => this.getUnitColor(d.properties.country),
-        getRadius: 1000,
-        onClick: d => {
-          if(d.object) {
-            this.$router.push(`/unitp/${d.object.properties.id}`)
-          }
-        },
-        onHover: d => this.setTooltipPos({x: d.x, y: d.y, object: d.object})
-      });
-    },
-    getUnitColor(name) {
-      const GER_COLOR = [173, 27, 27];
-      const ALLIED_COLOR = [0, 35, 149];
-      const UK_COLOR = [32, 142, 201];
-      const CA_COLOR = [221, 215, 215];
-      switch (name) {
-        case 'GE':
-          return GER_COLOR;
-        case 'CA':
-          return CA_COLOR;
-        case 'UK':
-          return UK_COLOR;
-        case 'US':
-          return ALLIED_COLOR;
-        default:
-          return ALLIED_COLOR;
-      };
     },
   }
 }
